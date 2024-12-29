44b980346882416098272a3c4c4694f46d28af52
freek100 <freeknld@gmail.com>
2024-12-29 14:54:25 +0100

    Extended point class

--- util/include/point.h
+++ util/include/point.h
@@ -33,7 +33,7 @@ public:
         return Point(x-rhs.x, y-rhs.y);
     }
 
-    Point operator * (const size_t& scalar) const {
+    Point operator * (const long long& scalar) const {
         return Point(scalar*x, scalar*y);
     }
 
@@ -49,12 +49,39 @@ public:
         return *this;
     }
 
+    bool isNeighbour(const Point& point, bool includeDiagonal = false) const{
+        if (!includeDiagonal){
+            return (point==north() || point==east() || point==south() || point==west());
+        } else {
+            return (isNeighbour(point, false) || point==northeast() || point==southeast() || point==southwest() || point==northwest());
+        }
+    }
+
     Point north() const { return Point(x,y-1);}
     Point east() const { return Point(x+1,y);}
     Point south() const { return Point(x,y+1);}
     Point west() const { return Point(x-1,y);}
-    size_t x;
-    size_t y;
+    // Not the fastest, but is robust to direction definition changes
+    Point northeast() const { return north().east();}
+    Point southeast() const { return south().east();}
+    Point southwest() const { return south().west();}
+    Point northwest() const { return north().west();}
+
+    std::vector<Point> neighbours(bool includeDiagonal = false) const {
+        std::vector<Point> list;
+        list.push_back(north());
+        if (includeDiagonal){ list.push_back(northeast());}
+        list.push_back(east());
+        if (includeDiagonal){ list.push_back(southeast());}
+        list.push_back(south());
+        if (includeDiagonal){ list.push_back(southwest());}
+        list.push_back(west());
+        if (includeDiagonal){ list.push_back(northwest());}
+        return list;
+    }
+
+    long long x;
+    long long y;
 };
 inline std::ostream& operator<<(std::ostream& os, const Point& p){
     os << '(' << p.x << ',' << p.y << ')';
