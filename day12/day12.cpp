e0931a900fb32c047a0028ab27dbf4af50d6f281
Freek Acer <freeknld@gmail.com>
2024-12-30 10:49:08 +0100

    day12

--- day12/day12.cpp
+++ day12/day12.cpp
@@ -6,6 +6,7 @@
 using namespace aoc;
 std::pair<std::vector<Point>, size_t> walkRegion(const Map& input, const Point& start, std::set<Point>& visited);
 void walkHelper(const Map& input, const Point& current, std::set<Point>& visited, std::vector<Point>& region, size_t& perimeter, const char& type);
+
 int main(int argc, char *argv[]){
     // Read input
     parseArguments(argc, argv);
@@ -24,7 +25,39 @@ int main(int argc, char *argv[]){
     printSolution(totalPrice, false);
 
     // Part 2
-    printSolution(0, true);
+    // Tip from the subreddit: count corners instead of sides, they are actually equal
+    size_t price = 0;
+    size_t area;
+    size_t sides;
+    char type;
+    for (const auto& region: regions){
+        area = region.size();
+        sides = 0;
+        type = input[region[0]];
+        for (const auto& point: region){
+            bool north; bool east; bool south; bool west; // Bools store which points are different
+            north = (!input.isInside(point.north()) || type!=input[point.north()]);
+            east = (!input.isInside(point.east()) || type!=input[point.east()]);
+            south = (!input.isInside(point.south()) || type!=input[point.south()]);
+            west = (!input.isInside(point.west()) || type!=input[point.west()]);
+            if (north+east+south+west == 4){ sides=4;} // Single element region
+            else if (north+east+south+west == 3){ sides+=2;} // Two corners at one region
+            else if (north&&east || east&&south || south&&west || west&&north) { sides++;}
+
+            // Check inside corners
+            bool northeast; bool southeast; bool southwest; bool northwest; // Bools store which points are different
+            northeast = (!input.isInside(point.northeast()) || type!=input[point.northeast()]);
+            southeast = (!input.isInside(point.southeast()) || type!=input[point.southeast()]);
+            southwest = (!input.isInside(point.southwest()) || type!=input[point.southwest()]);
+            northwest = (!input.isInside(point.northwest()) || type!=input[point.northwest()]);
+            if (northeast && !north && !east) {sides++;}
+            if (southeast && !south && !east) {sides++;}
+            if (southwest && !south && !west) {sides++;}
+            if (northwest && !north && !west) {sides++;}
+        }
+        price+= area*sides;
+    }
+    printSolution(price, true);
     return 0;
 }
 
